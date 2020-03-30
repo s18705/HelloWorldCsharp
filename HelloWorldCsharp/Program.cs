05c4c98cec680d7861257ee07f9507cf6ce8a959
Karolina <61821714+s18705@users.noreply.github.com>
2020-03-31 00:49:05 +0200

    Dodaj pliki projektów.

---
+++ HelloWorldCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
